a0e1ec177590d923f64acf81af2a7e92d3eea3ec
kmykz <kamyarkazari@gmail.com>
2021-04-25 23:24:04 +0430

    Implemented isDeckValid in GameDeck.java

--- Project/src/main/java/game/GameDeck.java
+++ Project/src/main/java/game/GameDeck.java
@@ -1,6 +1,7 @@
 package game;
 // TODO : KAMYAR
 
+import card.Card;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -12,12 +13,22 @@ public class GameDeck {
     private Deck sideDeck;
     private String name;
     private User user;
-    public GameDeck(String name , User user) {
+
+    public GameDeck(String name, User user) {
         this.name = name;
         this.user = user;
     }
+
     public boolean isDeckValid() {
-        return false;
+        if (mainDeck.getCardsList().size() < 40 || mainDeck.getCardsList().size() > 60) return false;
+        if (sideDeck.getCardsList().size() > 15) return false;
+        for (Card i : mainDeck.getCardsList()) {
+            if (mainDeck.getCardCount(i.getCardName()) + sideDeck.getCardCount(i.getCardName()) > 3) return false;
+        }
+        for (Card i : sideDeck.getCardsList()) {
+            if (sideDeck.getCardCount(i.getCardName()) > 3) return false;
+        }
+        return true;
     }
 
 }
