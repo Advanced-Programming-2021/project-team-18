0b2a39a510b93ed32a405d5f2087e25bab43367e
kamyarkazari <kamyarkazari@gmail.com>
2021-05-06 11:43:08 +0430

    showBoard is now implemented

--- Project/src/main/java/data/Printer.java
+++ Project/src/main/java/data/Printer.java
@@ -20,28 +20,79 @@ public class Printer {
     public static void prompt(String message) {
         System.out.println(message);
     }
-    public static void showBoard(Player player) {
-        // TODO
+
+    public static void showBoard(Player player, Player opponent) {
+        System.out.println(opponent.getUser().getNickname() + ":" + opponent.getLifePoint());
+        int handSize = opponent.getHand().getCardsList().size();
+        for (int i = 0; i < handSize; i++) System.out.print("\tC");
+        System.out.println("\n" + opponent.getRemainingDeck().getCardsList().size());
+        int[] opponentSequence = {4, 2, 1, 3, 5};
+        showSpellsField(opponent, opponentSequence);
+        System.out.print("\n");
+        showMonstersField(opponent, opponentSequence);
+        System.out.print(opponent.getGraveyard().getCardsList().size() + "\t\t\t\t\t");
+        SpellCard spellCard = opponent.getFieldZone();
+        if (spellCard == null) System.out.println("O");
+        else System.out.println("E");
+        System.out.println("\n--------------------------\n");
+        int[] playerSequence = {5, 3, 1, 2, 4};
+        SpellCard spellCard2 = player.getFieldZone();
+        if (spellCard2 == null) System.out.print("O");
+        else System.out.print("E");
+        System.out.println("\t\t\t\t\t" + player.getGraveyard().getCardsList().size());
+        showMonstersField(player, playerSequence);
+        showSpellsField(player, playerSequence);
+        System.out.println("\t\t\t\t\t\t" + player.getRemainingDeck().getCardsList().size());
+        handSize = player.getHand().getCardsList().size();
+        for(int i = 0 ; i < handSize ; i++){
+            System.out.print("C\t");
+        }
+        System.out.print("\n");
+        System.out.println(player.getUser().getNickname() + ":" + player.getLifePoint());
     }
+
+    private static void showMonstersField(Player player, int[] playerSequence) {
+        for (int i = 0; i < 5; i++) {
+            MonsterCard monster = player.getMonstersFieldList()[playerSequence[i]];
+            Card card = (Card) monster;
+            if (monster == null) System.out.print("\tE");
+            else if (!monster.isDefenseMode() && card.isFaceUp()) System.out.print("\tOO");
+            else if (monster.isDefenseMode() && card.isFaceUp()) System.out.print("\tDO");
+            else System.out.print("\tDH");
+        }
+        System.out.print("\n");
+    }
+
+    private static void showSpellsField(Player player, int[] playerSequence) {
+        for (int i = 0; i < 5 ; i++) {
+            Card spellOrTrap = player.getSpellsAndTrapFieldList()[playerSequence[i]];
+            if (spellOrTrap == null) System.out.print("\tE");
+            else if (spellOrTrap.isFaceUp()) System.out.print("\tO");
+            else System.out.print("\tH");
+        }
+    }
+
     public static void showGraveyard(Deck deck) {
 
     }
-    public static void showDeck(GameDeck gameDeck , boolean isSideDeck) {
+
+    public static void showDeck(GameDeck gameDeck, boolean isSideDeck) {
         String result = "";
         result += "Deck: " + gameDeck.getName() + "\n";
         result += (isSideDeck ? "Side" : "Main") + "deck:\n";
         Deck deck = gameDeck.getMainDeck();
-        if(isSideDeck) deck = gameDeck.getSideDeck();
+        if (isSideDeck) deck = gameDeck.getSideDeck();
         result += "Monsters:\n";
-        for(Card card : deck.getCardsList())
-            if(card instanceof MonsterCard)
+        for (Card card : deck.getCardsList())
+            if (card instanceof MonsterCard)
                 result += card.getCardName() + ": " + card.getCardDescription() + "\n";
         result += "Spells and Traps:\n";
-        for(Card card : deck.getCardsList())
-            if(card instanceof SpellCard || card instanceof TrapCard)
+        for (Card card : deck.getCardsList())
+            if (card instanceof SpellCard || card instanceof TrapCard)
                 result += card.getCardName() + ": " + card.getCardDescription() + "\n";
         Printer.prompt(result);
     }
+
     public static void showScoreBoard() {
         List<User> users = User.getAllUsers();
         Collections.sort(users);
