1e28eb3590befdbad6ccda7e3608c3cd93b6c056
kamyarkazari <kamyarkazari@gmail.com>
2021-05-16 21:02:13 +0430

    Implemented SpellAbsorptionEffect.java

--- Project/src/main/java/effects/SpellAbsorptionEffect.java
+++ Project/src/main/java/effects/SpellAbsorptionEffect.java
@@ -1,15 +1,33 @@
 package effects;
 
+import card.Card;
+import card.SpellCard;
+import events.CardEvent;
+import events.CardEventInfo;
 import events.Event;
+import game.Player;
 
 public class SpellAbsorptionEffect extends Effect {
+
+    Player player;
 
     public void runEffect() {
 
     }
 
     public boolean permit(Event event) {
-
-        return false;
+        if (event instanceof CardEvent) {
+            Card sourceCard = ((CardEvent) event).getCard();
+            CardEventInfo info = ((CardEvent) event).getInfo();
+            if (info == CardEventInfo.ACTIVATE_EFFECT && sourceCard.hasEffect(this)) {
+                player = sourceCard.getPlayer();
+                return true;
+            }
+            else if (info == CardEventInfo.ACTIVATE_EFFECT && sourceCard instanceof SpellCard){
+                player.setLifePoint(player.getLifePoint() + 500);
+                return  true;
+            }
+        }
+        return true;
     }
 }
