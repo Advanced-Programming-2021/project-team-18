5afaf00df963c3123d74170374f3f82a0b053140
Kalam <alipasha.montaseri@gmail.com>
2021-05-16 12:22:06 +0430

    NOTE: utility.adkPlayer() added for in game interactions and few effects implemented

--- Project/src/main/java/effects/DestroyEnemyMonsterEffect.java
+++ Project/src/main/java/effects/DestroyEnemyMonsterEffect.java
@@ -1,12 +1,31 @@
 package effects;
 
 import events.Event;
+import events.SpellTrapActivationEvent;
+import game.Player;
+
 // by Pasha
-public class DestroyEnemyMonsterEffect extends Effect{
-    private int monstersCount;
-
+// cards with this effect : [raigeki , Dark Hole]
+public class DestroyEnemyMonsterEffect extends Effect {
+    private boolean destroyBothPlayers;// should be initialized
+    private void destroyPlayer(Player player) {
+        for(int i = 1;i <= Player.getFIELD_SIZE();++ i)
+            if(player.getMonstersFieldList()[i] != null)
+                player.destroyMonster(player.getMonstersFieldList()[i]);
+    }
+    private void activateEffect() {
+        destroyPlayer(selfPlayer.getOpponent());
+        if(destroyBothPlayers)
+            destroyPlayer(selfPlayer);
+    }
     public boolean permit(Event event) {
-
+        initializeSelfCardWithEvent(event);
+        if(event instanceof SpellTrapActivationEvent) {
+            SpellTrapActivationEvent spellTrapActivationEvent = (SpellTrapActivationEvent) event;
+            if(spellTrapActivationEvent.getCard().hasEffect(this)) {
+                activateEffect();
+            }
+        }
         return true;
     }
 }
