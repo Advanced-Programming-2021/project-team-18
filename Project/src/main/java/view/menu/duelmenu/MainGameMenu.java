f53aa3203b054a63f4220a8b1517e9caa3c1c137
kamyarkazari <kamyarkazari@gmail.com>
2021-07-06 22:56:51 +0430

    an update to main_game.fxml to support avatars. MainGameMenu.java is now implemented as the controller of main_game.fxml

---
+++ Project/src/main/java/view/menu/duelmenu/MainGameMenu.java
@@ -0,0 +1,18 @@
+package view.menu.duelmenu;
+
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Text;
+import view.View;
+
+public class MainGameMenu extends View {
+    public ImageView cardImageView;
+    public Text cardTitle;
+    public Text cardDescription;
+    public GridPane FieldGridPane;
+    public Label firstPlayerTitle;
+    public Label secondPlayerTitle;
+    public ImageView firstPlayerAvatar;
+    public ImageView secondPlayerAvatar;
+}
