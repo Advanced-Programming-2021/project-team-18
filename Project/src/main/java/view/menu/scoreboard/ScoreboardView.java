b3703804635345fb18e0e82cc9a854fc98b388e0
Sina Imani <mr.sinaimani@gmail.com>
2021-07-08 23:23:30 +0430

    adjusted graphics.

--- Project/src/main/java/view/menu/scoreboard/ScoreboardView.java
+++ Project/src/main/java/view/menu/scoreboard/ScoreboardView.java
@@ -2,10 +2,9 @@ package view.menu.scoreboard;
 
 import game.User;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
-import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
-import lombok.Setter;
 import menus.MenuController;
 import view.View;
 import view.menu.mainmenu.MainMenuView;
@@ -28,29 +27,28 @@ public class ScoreboardView extends View {
     public void initialize() {
         List<User> usersList = new ArrayList<>(User.getAllUsers());
         Collections.sort(usersList);
-        //Collections.reverse(usersList);
         int size = Math.min(usersList.size(), 20);
         for (int i = 0; i < size; i++) {
             User user1 = usersList.get(i);
             if (user1.getUsername().contentEquals(user.getUsername())) {
-                Text indexText = textMaker(String.valueOf(i + 1));
-                Text usernameText = textMaker(user1.getNickname());
-                Text scoreText = textMaker(String.valueOf(user1.getScore()));
-                indexText.setStroke(Color.BLUE);
-                usernameText.setStroke(Color.BLUE);
-                scoreText.setStroke(Color.BLUE);
-                gridPane.add(indexText, 0, i);
+                Label indexLabel = numericLabel(String.valueOf(i + 1));
+                Label usernameText = new Label(user1.getNickname());
+                Label scoreText = new Label(String.valueOf(user1.getScore()));
+                gridPane.add(indexLabel, 0, i);
                 gridPane.add(usernameText, 1, i);
                 gridPane.add(scoreText, 2, i);
+                continue;
             }
-            gridPane.add(textMaker(String.valueOf(i + 1)), 0, i);
-            gridPane.add(textMaker(user1.getNickname()), 1, i);
-            gridPane.add(textMaker(String.valueOf(user1.getScore())), 2, i);
+            gridPane.add(numericLabel(String.valueOf(i + 1)), 0, i);
+            gridPane.add(new Label(user1.getNickname()), 1, i);
+            gridPane.add(new Label(String.valueOf(user1.getScore())), 2, i);
         }
     }
 
-    private Text textMaker(String context) {
-        return new Text(context);
+    private Label numericLabel(String text) {
+        Label label = new Label(text);
+        label.getStyleClass().add("number-label");
+        return label;
     }
 
     public void back() throws IOException {
