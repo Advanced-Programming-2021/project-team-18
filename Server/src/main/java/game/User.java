e425b432d37382d9c2df12c9aec75856a4c1c1d9
kamyarkazari <kamyarkazari@gmail.com>
2021-07-21 13:49:10 +0430

    small fix

--- Server/src/main/java/game/User.java
+++ Server/src/main/java/game/User.java
@@ -5,6 +5,7 @@ import com.google.gson.annotations.Expose;
 import javafx.scene.image.Image;
 import lombok.Getter;
 import lombok.Setter;
+import org.jetbrains.annotations.NotNull;
 import utility.Utility;
 import view.UtilityView;
 
@@ -50,12 +51,12 @@ public class User implements Comparable<User>, Serializable {
     @Getter
     @Expose
     private String token;
-    private ArrayList<GameDeck> decks;
+    private final ArrayList<GameDeck> decks;
     private final HashMap<String, Integer> cardCount;
     @Getter
-    private ArrayList<String> messages = new ArrayList<>();
+    private final ArrayList<String> messages = new ArrayList<>();
     @Getter
-    private ArrayList<Question> questions = new ArrayList<>();
+    private final ArrayList<Question> questions = new ArrayList<>();
 
 
     public static List<User> getAllUsers() {
@@ -124,6 +125,14 @@ public class User implements Comparable<User>, Serializable {
         avatarID = Utility.getARandomNumber(UtilityView.getAvatarNumbers()) + 1;
     }
 
+    public static int getOnlineCount() {
+        int count = 0;
+        for (User user : allUsers) {
+            if (user.isOnline()) count++;
+        }
+        return count;
+    }
+
 
     public GameDeck getGameDeckByName(String name) {
         for (GameDeck gameDeck : decks) {
@@ -169,6 +178,10 @@ public class User implements Comparable<User>, Serializable {
         cardCount.put(cardName, cardCount.getOrDefault(cardName, 0) + 1);
     }
 
+    public void subtractCardBalance(String cardName) {
+        cardCount.put(cardName, cardCount.getOrDefault(cardName, 0) - 1);
+    }
+
     public int getCardBalance(String cardName) {
         return cardCount.getOrDefault(cardName, 0);
     }
@@ -199,14 +212,6 @@ public class User implements Comparable<User>, Serializable {
         File dest = new File(new URI(Objects.requireNonNull(User.class.getResource("/avatars"))
                 + "/arbitrary/" + username));
         Files.copy(Paths.get(src.toURI()), Paths.get(dest.toURI()), REPLACE_EXISTING);
-    }
-
-    public static int getOnlineCount() {
-        int count = 0;
-        for (User user : allUsers) {
-            if (user.isOnline()) count++;
-        }
-        return count;
     }
 
     @Override
